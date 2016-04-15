07fda55b2270518953275594559f08ccd10d37a1
Takafumi Shibasaki <tkfmsb@gmail.com>
2016-04-15 19:23:20 +0900

    Fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  get 'api/callback' => 'webhook#callback'
+  post 'api/callback' => 'webhook#callback'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
